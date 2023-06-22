b45ff31e2bcc0f724d92f29d9a34cc8ad6698a23
Your Name <sharunnd11@gmail.com>
2023-06-23 00:32:44 +0530

    login and signup page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
+import Footer from "./components/Footer";
 import Home from "./components/Home";
+import { AllRoutes } from "./routes/AllRoutes";
 
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <AllRoutes />
+      <Footer />
     </div>
   );
 }
