93daa4bb54d2934cfc9583511e63b1f1b35126ef
AAdi23456 <118712556+AAdi23456@users.noreply.github.com>
2023-06-24 01:19:02 +0530

    docs added

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,31 @@
 import express from 'express'
 import AIroutes from "./routes/openai.js"
-import github from "./routes/github.rotes.js"
 import cors from 'cors'
 const app=express()
 import connection from './DataBase/DB.js'
 import Authorization from './routes/UserRegister.js'
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from "swagger-ui-express"
+const swaggerOptions = {
+
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Your API',
+      version: '1.0.0',
+      description: 'API documentation for Your App',
+    },
+    basePath: '/',
+  },
+  apis: ['routes/*.js'],
+};
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
 app.use(express.json())
 app.use(cors())
 
 app.use("/interview",AIroutes)
 app.use("/auth",Authorization)
-app.use("/git",github)
-
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.listen(8080, () => {
   try {
       connection()
