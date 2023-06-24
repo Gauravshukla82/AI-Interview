1f52d45f6251b7b43b492b3268e9ead27a72a16d
Asim <mohdasim.srk@gmailcom>
2023-06-24 12:01:49 +0530

    fw24_477_part-2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 
 import Footer from "./components/Footer";
-import Home from "./components/Home";
 import { AllRoutes } from "./routes/AllRoutes";
 
 
@@ -8,8 +7,7 @@ import { AllRoutes } from "./routes/AllRoutes";
 
 function App() {
   return (
-    <div className="App">
-
+    <div >
       <AllRoutes />
       <Footer />
 
--- frontend/src/routes/AllRoutes.jsx
+++ frontend/src/routes/AllRoutes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Home from "../components/Home"
 import Login from "../components/Login"
 import Register from "../components/Register"
+import Chat from "../components/Chat"
 
 const AllRoutes = () => {
     return (
@@ -9,6 +10,8 @@ const AllRoutes = () => {
             
             <Routes>
                 <Route path='/' element={<Home />}/>
+                <Route path='/sample' element={<sample />}/>
+                <Route path='/chat' element={<Chat />}/>
                 <Route path='/login' element={<Login />}/>
                 <Route path='/register' element={<Register />}/>
             </Routes>
